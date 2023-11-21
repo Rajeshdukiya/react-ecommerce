33c24072dc5183c4e9dd59f0f78809e54e9f6776
RAJESHDUKIYA <rajeshdukiya2000@gmail.com>
2023-11-21 21:51:04 +0530

    filter and sort and pagination changes

--- src/features/navbar/Navbar.jsx
+++ src/features/navbar/Navbar.jsx
@@ -38,7 +38,7 @@ function NavBar({ children }) {
                 <div className="flex h-16 items-center justify-between">
                   <div className="flex items-center">
                     <div className="flex-shrink-0">
-                    <Link to="/">
+                      <Link to="/">
                       <img
                         className="h-8 w-8"
                         src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
